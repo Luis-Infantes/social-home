df0c3f66ca3437892ae34dd3cd377d252c28e87c
Luis Infantes <Luis@InfantesArtdesign.onmicrosoft.com>
2025-09-18 18:17:13 +0200

    Diseño de la sección datos de perfil de perfil.html

--- perfil/perfil-perfil.js
+++ perfil/perfil-perfil.js
@@ -1,11 +1,49 @@
-//-------------------PERFIL--------------------
+//-------------------perfil.html--------------------
 
-const perfilHeader = document.getElementById('social-header')
 
-const nombreUser = localStorage.getItem('usuario') ;
+// ------------Funcion para los datos del perfil-------//
 
-const saludo = document.createElement('h1');
-saludo.classList.add('saludo-user');
-saludo.textContent = `${nombreUser}`;
+const personalDates = {
 
-perfilHeader.appendChild(saludo);
+    date: document.getElementById('user-info'),
+
+    content: function(key, value) {
+
+        const element = document.createElement('p');
+
+        if (key.toLowerCase() === 'email') {
+
+            element.innerHTML = `${key}: <a href="${value}" class="user-email">${value}</a>`;
+        } else {
+
+            element.textContent = `${key}: ${value}`;
+        }
+
+        this.date.appendChild(element);
+
+        
+
+    }
+};
+
+const userName = localStorage.getItem('usuario');
+const property = 'Tercero D';
+const email = 'Luis@contact.es';
+const tlfNumber = '555 55 55 55';
+const userWork = 'Activo';
+const userNote = 'Me encantan las juntas de vecinos. No me pierdo ninguna';
+
+
+
+
+personalDates.content('Nombre', userName);
+personalDates.content('Piso', property);
+personalDates.content('email', email);
+personalDates.content('Tlf', tlfNumber);
+personalDates.content('Trabajo', userWork);
+personalDates.content('Frase', userNote);
+
+
+
+
+
